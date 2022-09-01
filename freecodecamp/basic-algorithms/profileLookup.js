ec080752427a4b40534d04b2c044e669b2611a7a
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-01 21:53:41 +0200

    passed all tests

--- freecodecamp/basic-algorithms/profileLookup.js
+++ freecodecamp/basic-algorithms/profileLookup.js
@@ -11,42 +11,48 @@
 
 // Setup
 const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
-  
-  function lookUpProfile(name, prop) {
-    // Only change code below this line
-  
-    for (let i = 0; i < contacts.length; i++) {
-      if (contacts[i].firstName === name) {
-        return contacts[i].likes;
-      }
-    }
-  
-    // Only change code above this line
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i];
+
+    if (contact.firstName === name) {
+      if (contact[prop] != undefined) {
+      return contact[prop];
+    } 
+    return "No such property";
   }
-  console.log(lookUpProfile("Akira", "likes"));
-  lookUpProfile("Akira", "likes");
+}
+      return "No such contact";
+
+  // Only change code above this line
+}
+console.log(lookUpProfile("Akira", "likes"));
+lookUpProfile("Akira", "likes");
