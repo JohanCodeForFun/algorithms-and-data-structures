8fd0866060a1453b307cf2b269bace5126359ce6
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-04 23:13:19 +0200

    Create accessPropertiesWithDotNotation.js

---
+++ freecodecamp/object-oriented-programming/accessPropertiesWithDotNotation.js
@@ -0,0 +1,10 @@
+// Use Dot Notation to Access the Properties of an Object
+// Dot notation is used on the object name, duck, followed by the name of the property, name, to access the value of Aflac.
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+// Only change code below this line
+console.log(dog.name);
+console.log(dog.numLegs);
