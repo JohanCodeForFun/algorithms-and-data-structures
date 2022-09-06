001616043101b69d4c77a29026666aa704e7bfeb
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-06 23:20:54 +0200

    Update catchOffByOne-OBOE.js

--- freecodecamp/debugging/catchOffByOne-OBOE.js
+++ freecodecamp/debugging/catchOffByOne-OBOE.js
@@ -1,5 +1,13 @@
 // Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
 
+let firstFive = "12345";
+let len = firstFive.length;
+// Only change code below this line
+for (let i = 0; i < len; i++) {
+  // Only change code above this line
+  console.log(firstFive[i]);
+}
+
 //before
 // function countToFive() {
 //     let firstFive = "12345";
@@ -12,11 +20,3 @@
 //   }
 
 //   countToFive();
-
-let firstFive = "12345";
-let len = firstFive.length;
-// Only change code below this line
-for (let i = 0; i < len; i++) {
-  // Only change code above this line
-  console.log(firstFive[i]);
-}
