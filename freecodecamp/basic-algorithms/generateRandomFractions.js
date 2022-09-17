aaadaf3308a77fe807ee70ae5fdcd241240d4c55
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-17 21:02:39 +0200

    Update generateRandomFractions.js

--- freecodecamp/basic-algorithms/generateRandomFractions.js
+++ freecodecamp/basic-algorithms/generateRandomFractions.js
@@ -1,3 +1,6 @@
+// Generate Random Fractions with JavaScript
+// Change randomFraction to return a random number instead of returning 0.
+
 function randomFraction() {
 
     // Only change code below this line
