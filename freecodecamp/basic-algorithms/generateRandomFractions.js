b17e67bfa2d8e52a3e0925af6ffd97ed58f3f42b
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-17 19:52:24 +0200

    Create generateRandomFractions.js

---
+++ freecodecamp/basic-algorithms/generateRandomFractions.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+
+    // Only change code below this line
+  
+    return Math.random();
+
+    // before
+    // return 0;
+  
+    // Only change code above this line
+  }
