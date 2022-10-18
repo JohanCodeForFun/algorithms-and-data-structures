fbc0022b1c35be8cfd00c3af34df92cc2fc9f2c7
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-18 12:35:55 +0200

    Update CreateStringsUsingTemplateLiterals.js

--- freecodecamp/ES6/CreateStringsUsingTemplateLiterals.js
+++ freecodecamp/ES6/CreateStringsUsingTemplateLiterals.js
@@ -2,21 +2,22 @@
 // Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
 
 const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-  };
-  function makeList(arr) {
-    // Only change code below this line
-    const failureItems = [];
-    for (let i = 0; i < result.length; i++) {
-      console.log(result[i]);
-    }
-  
-    console.log(failureItems);
-    // Only change code above this line
-  
-    return failureItems;
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  let failureArr = result.failure
+
+  for (const item of failureArr) {
+    let listItem = `<li class="text-warning">${item}</li>`
+    failureItems.push(listItem)
   }
-  
-  const failuresList = makeList(result.failure);
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
