f5eae84e343e231a3f265f1e83fb08e304096029
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-19 16:07:09 +0200

    Create booWho.js

---
+++ freecodecamp/basic-algorithms/booWho.js
@@ -0,0 +1,10 @@
+// Boo who
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
+
+let booWho = (bool) => typeof bool === "boolean";
+
+// alt,
+// function booWho(bool) {
+//   return typeof bool === "boolean";
+// }
