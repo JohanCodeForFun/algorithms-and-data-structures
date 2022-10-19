a4da7e08ffdf5c15a8feb09c23f576752dbea602
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-19 16:07:14 +0200

    Create FindersKeepers.js

---
+++ freecodecamp/basic-algorithms/FindersKeepers.js
@@ -0,0 +1,12 @@
+// Finders Keepers
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+let findElement = (arr, func) => arr.find(func);
+
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
+
+// alt,
+// function findElement(arr, func) {
+//   const found = arr.find(func);
+//   return found;
+// }
