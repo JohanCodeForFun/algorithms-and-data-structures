3cd9283113c22eeb7dfc278dc6bd123496518d07
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-12-08 21:01:10 +0100

    Return a Sorted Array not mutating Original Array

---
+++ freecodecamp/functional-programming/ReturnaSortedArrayWithoutChangingtheOriginalArray.js
@@ -0,0 +1,14 @@
+// Return a Sorted Array Without Changing the Original Array
+// Use the sort method in the nonMutatingSort function to sort the elements of an array in ascending order. The function should return a new array, and not mutate the globalArray variable.
+
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  const newArr = [...arr].sort((a, b) => a -b)
+
+  return newArr
+  // Only change code above this line
+}
+
+console.log(nonMutatingSort(globalArray));
