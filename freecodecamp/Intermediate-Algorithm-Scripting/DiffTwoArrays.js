4addefb243bfba02e71e76721ca56db621e9d2a0
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-12-19 22:10:01 +0100

    Diff Two Arrays

--- freecodecamp/Intermediate-Algorithm-Scripting/DiffTwoArrays.js
+++ freecodecamp/Intermediate-Algorithm-Scripting/DiffTwoArrays.js
@@ -3,21 +3,35 @@
 // Note: You can return the array with its elements in any order.
 
 function diffArray(arr1, arr2) {
-  const newArr = [];
-
-  function compareArr(first, second) {
-    first.forEach(number => {
-      if (second.indexOf(number) === -1) {
-        newArr.push(number)
-      }
-    });
-  }
-
-  compareArr(arr1, arr2)
-  compareArr(arr2, arr1)
-
-
-  return newArr;
+  const newArr = [...arr1, ...arr2];
+  const result = newArr.filter(item => {
+    if (arr1.includes(item) != arr2.includes(item)) {
+      return item
+    }
+  })
+  return result;
 }
 
+let result = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(result);
 console.log(diffArray(["diorite", "andesite", "grass", "dirt", "dead shrub"], ["diorite", "andesite", "pink wool", "grass", "dirt", "dead shrub"]));
+
+
+// Another solution, 
+// function diffArray(arr1, arr2) {
+//   const newArr = [];
+
+//   function compareArr(first, second) {
+//     first.forEach(number => {
+//       if (second.indexOf(number) === -1) {
+//         newArr.push(number)
+//       }
+//     });
+//   }
+
+//   compareArr(arr1, arr2)
+//   compareArr(arr2, arr1)
+
+
+//   return newArr;
+// }
