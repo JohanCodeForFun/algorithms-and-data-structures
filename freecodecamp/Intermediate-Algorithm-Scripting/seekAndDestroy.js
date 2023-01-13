30f9a0eaa2cadfd0509784f0feac6f78f1e63b5b
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-13 20:49:37 +0100

    Seek and Destroy all elements from initial array

---
+++ freecodecamp/Intermediate-Algorithm-Scripting/seekAndDestroy.js
@@ -0,0 +1,19 @@
+// Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+function destroyer(arr) {
+  const targets = Array.prototype.slice.call(arguments[0]);
+  const destroyer = Array.prototype.slice.call(arguments, 1);
+  const survivors = [];
+
+  const seek = targets.filter((element) => {
+    if (!destroyer.includes(element)) {
+      survivors.push(element);
+    }
+  });
+
+  return survivors;
+}
+
+const result = destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+console.log(result);
