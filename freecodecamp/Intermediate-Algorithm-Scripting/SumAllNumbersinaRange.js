5811db6b3aac64e6b8744701d0318d2e498d4f21
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-13 12:41:41 +0100

    add another solution using reduce

--- freecodecamp/Intermediate-Algorithm-Scripting/SumAllNumbersinaRange.js
+++ freecodecamp/Intermediate-Algorithm-Scripting/SumAllNumbersinaRange.js
@@ -1,17 +1,35 @@
 // Sum All Numbers in a Range
 // For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 
+// one way to solve it with reduce
+function sumAll(arr) {
+  let sumArr = [];
+  const maxValue = Math.max(...arr);
+  const minValue = Math.min(...arr);
+
+  for (let i = minValue; i <= maxValue; i++) {
+    sumArr.push(i);
+  }
+
+  const result = sumArr.reduce((acc, cur) => acc + cur, 0);
+
+  return result;
+}
+
+const result = sumAll([1, 4]);
+console.log(result);
+
+// another way to solve in within for-loop
 function sumAll(arr) {
   let result = 0;
-  let lowestNum = Math.min(arr[0], arr[1])
-  let maxNum = Math.max(arr[0], arr[1])
+  let lowestNum = Math.min(arr[0], arr[1]);
+  let maxNum = Math.max(arr[0], arr[1]);
 
   for (let i = lowestNum; i <= maxNum; i++) {
     result += i;
   }
 
-  return result
-
+  return result;
 }
 
 console.log(sumAll([1, 4]));
