aa885f07eea98a14029d3235afe9f937d65d2dda
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-26 15:59:44 +0100

    added for loop solution

--- freecodecamp/Intermediate-Algorithm-Scripting/SortedUnion.js
+++ freecodecamp/Intermediate-Algorithm-Scripting/SortedUnion.js
@@ -8,6 +8,29 @@ function uniteUnique(arr) {
   return uniqueSet;
 }
 
+// Solution with for loop
+// function uniteUnique(arr) {
+//   const result = [];
+
+//   // looping through array arguments
+//   for (let i = 0; i < arguments.length; i++) {
+//     const arrayArgs = arguments[i];
+
+//     // getting each value in array arguments
+//     for (let j = 0; j < arrayArgs.length; j++) {
+//       let index = arrayArgs[j];
+
+//       // checking if value exists in result array
+//       // push index value to result if not found.
+//       if (result.indexOf(index) < 0) {
+//         result.push(index);
+//       }
+//     }
+//   }
+
+//   return result;
+// }
+
 const result = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 // should return [1, 3, 2, 5, 4]
 console.log(result);
