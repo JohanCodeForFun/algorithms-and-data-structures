a123d19e13daa1a20b2d19c7c35108e82fc69d0c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-26 10:38:10 +0100

    Pairs of DNA strands consist of nucleobase pairs.

---
+++ freecodecamp/Intermediate-Algorithm-Scripting/DNAPairing.js
@@ -0,0 +1,35 @@
+// DNA Pairing
+// Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+// The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+// For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+// The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+function pairElement(str) {
+  const result = [];
+
+  const dnaInput = str.split("");
+
+  dnaInput.forEach((dnaStrand) => {
+    switch (dnaStrand) {
+      case "A":
+        result.push(["A", "T"]);
+        break;
+      case "T":
+        result.push(["T", "A"]);
+        break;
+      case "C":
+        result.push(["C", "G"]);
+        break;
+      case "G":
+        result.push(["G", "C"]);
+        break;
+    }
+    return result;
+  });
+  return result;
+}
+
+const result = pairElement("ATCGA");
+console.log(result);
