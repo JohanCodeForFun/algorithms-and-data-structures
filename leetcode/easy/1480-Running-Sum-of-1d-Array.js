c9d83bc686f93d73a03e06a8665533a7ff2970e9
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-27 16:35:38 +0100

    added multiple solutions

--- leetcode/easy/1480-Running-Sum-of-1d-Array.js
+++ leetcode/easy/1480-Running-Sum-of-1d-Array.js
@@ -14,6 +14,32 @@ const runningSum = (nums) => {
 };
 
 // solution with for loop
+// time complexity = 0(n)
+// space complexity = 0(1)
+
+// const runningSum = (nums) => {
+//   for (let i = 1; i < nums.length; i++) {
+//     nums[i] += nums[i - 1];
+//   }
+//   return nums;
+// };
+
+// another solution with stored values
+// const runningSum = (nums) => {
+//   const startValue = nums[0];
+//   const resultArray = [];
+//   let temp = 0;
+
+//   resultArray.push(startValue);
+//   for (let i = 1; i < nums.length; i++) {
+//     console.log(nums[i]);
+//     temp = nums[i] + resultArray[i - 1];
+//     resultArray.push(temp);
+//   }
+//   return resultArray;
+// };
+
+// another solution with for loop
 // var runningSum = function (nums) {
 //   let currentSum = nums[0];
 //   let sumNums = [];
