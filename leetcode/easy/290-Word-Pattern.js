95426c5e0baa5c6501fd692a24cf5adce1ceb9cd
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-29 21:35:17 +0100

    continued study on word pattern with map

--- leetcode/easy/290-Word-Pattern.js
+++ leetcode/easy/290-Word-Pattern.js
@@ -7,22 +7,83 @@
  * @param {string} s
  * @return {boolean}
  */
+
 var wordPattern = function (pattern, s) {
-  const dictionaryString = {};
-  const dictionaryPattern = {};
-  const stringArr = s.split(" ");
+  // place words into an array
+  sArr = s.split(" ");
 
-  for (let i = 0; i < stringArr.length; i++) {
-    if (dictionaryString[s[i]] !== dictionaryPattern[pattern[i]]) return false;
+  // if the pattern is lopsided return false
+  if (pattern.length !== sArr.length) return false;
 
-    dictionaryString[s[i]] = i;
-    dictionaryPattern[pattern[i]] = i;
-    console.log(dictionaryString, dictionaryPattern);
+  // create a key map and value map
+  /* as a caveat, we can't use one map here
+   * in the edge-case that a pattern key
+   * and values in s contain the same characters
+   *
+   * eg. pattern = 'abc', s = 'c b a'
+   */
+  const keyMap = {}; // key map
+  const vMap = {}; // value map
+
+  for (let i = 0; i < pattern.length; i++) {
+    const key = pattern[i]; // letter at index i in pattern
+    const value = sArr[i]; // word at index i in s
+
+    // if key or value already exists and does not correspond to
+    // eachother, return false because we don't have a valid pattern
+    if (key in keyMap || value in vMap) {
+      if (keyMap[key] !== value) return false;
+      if (vMap[value] !== key) return false;
+    } else {
+      keyMap[key] = value;
+      vMap[value] = key;
+    }
   }
-  return true;
+
+  return true; // pattern is valid
 };
 
-const case1 = wordPattern("abba", "dog cat cat dog");
+// solution with Map()
+// var wordPattern = function (pattern, str) {
+//   const words = str.split(/\s+/);
+//   const map = new Map();
+//   console.log(words);
+
+//   if (words.length !== pattern.length) return false;
+//   if (new Set(words).size !== new Set(pattern).size) return false;
+
+//   for (let i = 0; i < pattern.length; i++) {
+//     if (map.has(pattern[i]) && map.get(pattern[i]) !== words[i]) return false;
+//     map.set(pattern[i], words[i]);
+//   }
+//   return true;
+// };
+
+// var wordPattern = function (pattern, s) {
+//   const keyMap = {}; // dictionaryString
+//   const valueMap = {}; // dictionaryPattern
+//   const stringArr = s.split(" ");
+
+//   if (pattern.length !== stringArr.length) return false;
+
+//   for (let i = 0; i < pattern.length; i++) {
+//     const key = pattern[i];
+//     const value = stringArr[i];
+
+//     console.log(key, keyMap);
+//     if (key in keyMap || value in valueMap) {
+//       console.log(keyMap[key]);
+//       if (keyMap[key] !== value) return false;
+//       if (valueMap[value] !== key) return false;
+//     } else {
+//       keyMap[key] = value;
+//       valueMap[value] = key;
+//     }
+//   }
+//   return true;
+// };
+
+const case1 = wordPattern("abba", "dog constructor constructor dog");
 console.log(case1);
 // Input: pattern = "abba", s = "dog cat cat dog"
 // Output: true
