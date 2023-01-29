38b0a8a3c5ea588b441ec086198ea479f3f97380
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-29 21:49:10 +0100

    continued study of Isomorphic Strings

--- leetcode/top75/205-Isomorphic-Strings.js
+++ leetcode/top75/205-Isomorphic-Strings.js
@@ -10,26 +10,43 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isIsomorphic = function (s, t) {
-  const dictionaryS = {};
-  const dictionaryT = {};
-
+  if (s.length != t.length) return false;
+  let map = new Map();
   for (let i = 0; i < s.length; i++) {
-    if (dictionaryS[s[i]] !== dictionaryT[t[i]]) return false;
-
-    dictionaryS[s[i]] = i;
-    dictionaryT[t[i]] = i;
-    console.log(
-      "Line 18: ",
-      s.charAt(i),
-      dictionaryS[s[i]],
-      t.charAt(i),
-      dictionaryT[t[i]]
-    );
-    console.log(dictionaryS, dictionaryT);
+    if (!map.has(s[i])) map.set(s[i], t[i]);
+    else {
+      if (map.get(s[i]) != t[i]) {
+        return false;
+      }
+    }
   }
-  return true;
+  return new Set([...map.values()]).size == map.size;
 };
+
+// working on solution for this
+// var isIsomorphic = function (s, t) {
+//   const dictionaryS = {};
+//   const dictionaryT = {};
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (dictionaryS[s[i]] !== dictionaryT[t[i]]) return false;
+
+//     dictionaryS[s[i]] = i;
+//     dictionaryT[t[i]] = i;
+
+//     console.log(
+//       "Line 18: ",
+//       s.charAt(i),
+//       dictionaryS[s[i]],
+//       t.charAt(i),
+//       dictionaryT[t[i]]
+//     );
+//     console.log(dictionaryS, dictionaryT);
+//   }
+//   return true;
+// };
 
 // const case1 = isIsomorphic("egg", "add");
 // const case2 = isIsomorphic("foo", "bar");
