91228912a825e401ff845d00b3cd4c65245e0ea0
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-01 02:21:19 +0100

    Arguments Optional

---
+++ freecodecamp/Intermediate-Algorithm-Scripting/ArgumentsOptional.js
@@ -0,0 +1,17 @@
+// Arguments Optional
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+function addTogether() {
+  const [first, second] = arguments;
+  if (typeof first !== "number") return undefined;
+  if (arguments.length === 1) return (second) => addTogether(first, second);
+  if (typeof second !== "number") return undefined;
+  return first + second;
+}
+
+console.log(addTogether(2, 3)); // should return 5.
+console.log(addTogether(5)(7)); // should return 12.
+console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ")); // should return undefined;
+console.log(addTogether(2)([3])); // should return undefined.
+console.log(addTogether("2", 3)); // should return undefined.
+console.log(addTogether(5, undefined)); // should return undefined.
