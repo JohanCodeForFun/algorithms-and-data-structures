c092a851f55e57cdb470af43e1ab5172c5d4fe6d
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-01 02:23:26 +0100

    Added studie guide

--- freecodecamp/Intermediate-Algorithm-Scripting/ArgumentsOptional.js
+++ freecodecamp/Intermediate-Algorithm-Scripting/ArgumentsOptional.js
@@ -1,5 +1,7 @@
 // Arguments Optional
 // Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+// Studied solution from Useful Programmer and Dylan Israel @ youtube.
+// Studied solution from hint section.
 
 function addTogether() {
   const [first, second] = arguments;
