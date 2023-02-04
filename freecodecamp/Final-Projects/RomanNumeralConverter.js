26daa51738209caaffa7184c15d6ef25d0f598de
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-04 20:40:30 +0100

    Roman Numeral Converter - Final Project #2 - DONE

---
+++ freecodecamp/Final-Projects/RomanNumeralConverter.js
@@ -0,0 +1,56 @@
+// Roman Numeral Converter - Final Project #2
+// Convert the given number into a roman numeral.
+
+// Roman Numeral Converter - Final Project #2
+// Convert the given number into a roman numeral.
+
+function convertToRoman(num) {
+  let accumulator = "";
+  const romanLookUp = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  for (const key in romanLookUp) {
+    let romanNumberValue = romanLookUp[key];
+
+    while (romanNumberValue <= num) {
+      num -= romanNumberValue;
+      accumulator += key;
+    }
+  }
+
+  return accumulator;
+}
+
+console.log(convertToRoman(36));
+
+// Tests
+// convertToRoman(2) should return the string II.
+// convertToRoman(3) should return the string III.
+// convertToRoman(4) should return the string IV.
+// convertToRoman(5) should return the string V.
+// convertToRoman(9) should return the string IX.
+// convertToRoman(12) should return the string XII.
+// convertToRoman(16) should return the string XVI.
+// convertToRoman(29) should return the string XXIX.
+// convertToRoman(44) should return the string XLIV.
+// convertToRoman(45) should return the string XLV.
+// convertToRoman(68) should return the string LXVIII
+// convertToRoman(83) should return the string LXXXIII
+// convertToRoman(97) should return the string XCVII
+// convertToRoman(99) should return the string XCIX
+// convertToRoman(400) should return the string CD
+// convertToRoman(500) should return the string D
+// convertToRoman(501) should return the string DI
