b08087b19383ad3ad6a0d829bec43b7964d4d9f4
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-08 21:13:11 +0100

    Telephone Number Validator - Final Project #4

--- freecodecamp/Final-Projects/TelephoneNumberValidator.js
+++ freecodecamp/Final-Projects/TelephoneNumberValidator.js
@@ -2,25 +2,39 @@
 // Return true if the passed string looks like a valid US phone number.
 
 function telephoneCheck(str) {
-  const telephoneCheckCase2 = /(\d{3})\1\s|-\1(\d{4})/;
-  // const telephoneCheckCase1 = /(\d{3})\2(\s|-)(\d{4})/;
-  // const telephoneCheckCase2 = /(\d{3})\2(\s|-)(\d{4})/; // use \2 to repeat
-  // const telephoneCheckCase1 = /(?=\d{3})(?=\d{3})(?=\d{4})/;
-  console.log(str.match(telephoneCheckCase2));
+  // one line
+  const validPhonePattern = /^1?\s?(\d{3}|\(\d{3}\))-?\s?\d{3}-?\s?\d{4}$/g;
 
-  return telephoneCheckCase2.test(str);
-  //   const personList = `First_Name: John, Last_Name: Doe
-  // First_Name: Jane, Last_Name: Smith`;
+  // return validPhonePattern.test(str);
 
-  //   const regexpNames = /First_Name: (\w+), Last_Name: (\w+)/gm;
-  //   for (const match of personList.matchAll(regexpNames)) {
-  //     console.log(`Hello ${match[1]} ${match[2]}`);
-  //   }
+  //
+  const validPhonePatterns = [
+    // 5555555555
+    /^\d{10}$/g,
+
+    // 555-555-5555
+    /^\d{3}-\d{3}-\d{4}$/g,
+
+    // 555 555 5555
+    // 1 555 555 5555
+    /^1?\s?\d{3}[-|\s]\d{3}[-|\s]\d{4}$/g,
+
+    // 1(555)555-5555
+    /^1?\(\d{3}\)\d{3}[-|\s]\d{4}$/g,
+
+    // 2 (757) 622-7382
+    /^1[^2|^0]\(\d{3}\)[\s|-]\d{3}[-|\s]\d{4}$/g,
+  ];
+  return validPhonePatterns.some((pattern) => pattern.test(str));
 }
 
+console.log(telephoneCheck("2 (757) 622-7382"));
+
 const case1 = telephoneCheck("555 555-5555");
-const case2 = telephoneCheck("555555 5555");
-const case3 = telephoneCheck("(555)555-5555");
-console.log(case1);
-console.log(case2);
-console.log(case3);
+// const case2 = telephoneCheck("555555 5555");
+// const case3 = telephoneCheck("(555)555-5555");
+const case4 = telephoneCheck("555-5555");
+// console.log(case1);
+// console.log(case2);
+// console.log(case3);
+console.log(case4);
